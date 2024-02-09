fe95dc43756e217826fe28a90194f2bc0e4caa95
Rafael Sabino <rafaelsabino@live.com>
2024-02-09 11:18:05 -0300

    correcao para implementar senha criptografada

--- src/controllers/updatePass.js
+++ src/controllers/updatePass.js
@@ -1,12 +1,13 @@
 import { updatePassService } from '../service/updateService.js';
+import bcrypt from 'bcrypt';
 
 const updatePass = async (req, res) => {
     const {id: client_id} = req.client
     const {client_pass} = req.body
     
     try {
-        
-        await updatePassService(client_pass, client_id);
+        const encryptedPass = await bcrypt.hash(client_pass, 10);
+        await updatePassService(encryptedPass, client_id);
 
         return res.status(204).send();
     } catch (err) {
