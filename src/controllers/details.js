fe7bee077d68819c5b2221d42c1205e6f4b6bef9
Rafael Sabino <rafaelsabino@live.com>
2024-02-20 17:30:33 -0300

    mostrar favoritos na rota de detalhes do client

--- src/controllers/details.js
+++ src/controllers/details.js
@@ -1,13 +1,19 @@
+import { getAllFavoritesService } from "../service/favoritesService.js";
+
+
 export const details = async (req, res) => {
-    const {client_name, client_email, client_cpf, client_account_number, client_balance} = req.client;
+    const {id: client_id, client_name, client_email, client_cpf, client_account_number, client_balance} = req.client;
     
-    try {        
+    try {
+        const client_favorites = await getAllFavoritesService(client_id);
+         
         const client = {
             client_name,
             client_email,
             client_account_number,
             client_cpf,
-            client_balance
+            client_balance,
+            client_favorites
         }
         return res.status(200).json(client);
     } catch (err) {
